69e2caf83e1a6f56669d152ce2c63fed4220a2fb
Bruno Oliveira <brunk1809@gmail.com>
2024-07-02 22:21:28 -0300

    project created

---
+++ src/main/java/dev/brunow/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package dev.brunow.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
