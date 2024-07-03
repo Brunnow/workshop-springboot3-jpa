a14576ac098c6884eae7fb5cec2658b89d2e794b
Bruno Oliveira <brunk1809@gmail.com>
2024-07-02 22:41:38 -0300

    User entity and resource

---
+++ src/main/java/dev/brunow/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package dev.brunow.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.brunow.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "maria", "maria@gmai","99999999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
