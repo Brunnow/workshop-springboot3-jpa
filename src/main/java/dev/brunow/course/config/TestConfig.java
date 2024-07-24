f2b362758cb70dec241eff35af0f56619b816751
Bruno Oliveira <brunk1809@gmail.com>
2024-07-24 10:48:09 -0300

    payment, one-to-one association

--- src/main/java/dev/brunow/course/config/TestConfig.java
+++ src/main/java/dev/brunow/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import dev.brunow.course.entities.Category;
 import dev.brunow.course.entities.Order;
 import dev.brunow.course.entities.OrderItem;
+import dev.brunow.course.entities.Payment;
 import dev.brunow.course.entities.Product;
 import dev.brunow.course.entities.User;
 import dev.brunow.course.entities.enums.OrderStatus;
@@ -75,7 +76,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2, oi3,oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepositoy.save(o1);
 	}
 
 }
