459e7f5b2671de800c847a4b8a68239959e51d51
Bruno Oliveira <brunk1809@gmail.com>
2024-07-30 20:43:21 -0300

    Exception handling - delete

--- src/main/java/dev/brunow/course/services/UserService.java
+++ src/main/java/dev/brunow/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import dev.brunow.course.entities.User;
 import dev.brunow.course.repositories.UserRepository;
+import dev.brunow.course.resources.exceptions.DatabaseException;
 import dev.brunow.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 		return repository.save(obj);
 }
 	public void delete (Long id) {
-		repository.deleteById(id);
+		try {
+		    repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
